cabd714672259d3f2b5975287c24c0724c9a741c
Csaba Hoch <csaba.hoch@gmail.com>
2010-11-19 20:10:25 +0100

    test_hkweb: tests for some server classes added
    [hkweb cleanup]
    
    The global variables hkweb.hkweb_ports and hkshell.hkshell_started have been
    added so that it is easy to decide whether hkshell and hkweb are running or
    they have to be started.

--- src/hkshell.py
+++ src/hkshell.py
@@ -1885,6 +1885,8 @@ def parse_args(args=None):
     (cmdl_options, args) = parser.parse_args(args)
     return cmdl_options, args
 
+hkshell_started = False
+
 def main(cmdl_options, args):
     """Initializes |hkshell|.
 
@@ -1936,6 +1938,8 @@ def main(cmdl_options, args):
     if cmdl_options.no_shell:
         return
 
+    hkshell_started = True
+
     while True:
 
         # Creating a shell to the user
@@ -1967,3 +1971,5 @@ def main(cmdl_options, args):
             break
         else:
             break
+
+    hkshell_started = False
