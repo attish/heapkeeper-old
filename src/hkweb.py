5c7a43ca33a414f3a453ffcb9f4393c88b911f95
Attila Nagy <nagy.attila.1984@gmail.com>
2011-01-08 11:47:04 +0100

    hkweb: added link to indices to add new root
    [new root]
    
    This solves a very important shortcoming of hkweb: prior to this,
    there was no obvious way to add any posts to an empty heap using hkweb
    only. (The earlier commits added a facility to add a new root, this
    commit makes this facility accessible to those users who rely on
    point-and-click methods.)

--- src/hkweb.py
+++ src/hkweb.py
@@ -412,6 +412,19 @@ class IndexGenerator(WebGenerator):
         """Initializator."""
         pass
 
+    def print_add_root_link(self):
+        """Prints a link to add a new root to an existing heap."""
+
+        add_root_link = \
+            [
+                "<div class='global-buttons'>",
+                "   <span class='button global-button'>",
+                "       <a href='new-root'>Add new root</a>",
+                "   </span>",
+                "</div>"
+            ]
+        return "\n".join(add_root_link)
+
     def print_main(self):
         """Prints the main part of the page.
 
@@ -420,6 +433,7 @@ class IndexGenerator(WebGenerator):
 
         return (self.print_searchbar(),
                 self.print_additional_header({}),
+                self.print_add_root_link(),
                 self.print_main_index_page(),
                 self.print_additional_footer({}),
                 self.print_js_links())
